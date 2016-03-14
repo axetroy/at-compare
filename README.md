d48913b2af1dd0d8139e4bbb8a9fca12b2dcbe7a
axetroy <troy450409405@gmail.com>
2016-03-15 01:04:57 +0800

    update

--- README.md
+++ README.md
@@ -2,26 +2,54 @@
 
 用于表单验证的指令，例如a>b，b<c...
 
-# 指令compare = '{expression}';
+# 安装
+
+```bash
+bower install at-compare --save
+```
+
+# 依赖
+
+依赖于`angular`
+
+引入模块`ngAnimate`和`atPromise`
+
+```js
+angular
+	.module('angularTestApp', ['at-compare'])
+```
+
+# 指令at-compare = '{expression}';
 
 支持的语法：
 
 ```js
-compare = 'a>b';
-compare = 'a>=b';
-compare = 'a<b';
-compare = 'a<=b';
-compare = 'a==b';
-compare = 'a===b';
-compare = 'a!=b';
-compare = 'a!==b';
+at-compare = 'a>b';
+at-compare = 'a>=b';
+at-compare = 'a<b';
+at-compare = 'a<=b';
+at-compare = 'a==b';
+at-compare = 'a===b';
+at-compare = 'a!=b';
+at-compare = 'a!==b';
+```
+
+a和a分别代表当前form内，表单的name名
+
+支持多个比较语法，使用分号``;``隔开
+
+```js
+at-compare = 'a!==b；b>c;d<a；'
 ```
 
 ## 应用场景
 
-* 修改密码
+###  修改密码
 
 > 通常会有3项，`旧密码`，`新密码`，`确认新密码`，使用这个指令，就可以简单的实现表单验证
+> 要确保
+> * 新密码不能和旧密码相同         `at-compare="newPassWord!==lPrice"`
+> * 确认密码要和新密码一致         `at-compare="confirmPwd===newPassWord"`
 
 ```html
 <form name="testForm">
@@ -31,13 +59,56 @@ compare = 'a!==b';
   </div>
   <div class="form-group">
     <label>新密码</label>
-    <input type="text" name="newPassWord" compare="newPassWord!==lPrice" ng-model="reset.newPassWord">
+    <input type="text" name="newPassWord" at-compare="newPassWord!==lPrice" ng-model="reset.newPassWord">
   </div>
   <div class="form-group">
     <label>确认密码</label>
-    <input type="text" name="confirmPwd" compare="confirmPwd===newPassWord" ng-model="reset.newPassWord">
+    <input type="text" name="confirmPwd" at-compare="confirmPwd===newPassWord" ng-model="reset.confirmPwd">
   </div>
 </form>
 ```
 
-* 以及各种对比大小，比如原价<=折后价,等
+### 以及各种对比大小
+
+> 比如这里有个``最低价``、``原价``和``折后价``
+> 如果要确保
+> * 原价>=最低价                  ``at-compare="oPrice>lPrice"``
+> * 折后价<=原价 && 折后价>最低价   ``at-compare="price<=oPrice;price>lPrice"``
+
+```html
+<form name="testForm">
+  <div class="form-group">
+    <label>最低价</label>
+    <input type="text" name="lPrice" ng-model="item.lPrice">
+  </div>
+  <div class="form-group">
+    <label>原价</label>
+    <input type="text" at-compare="oPrice>lPrice" name="oPrice" ng-model="item.oProice">
+  </div>
+  <div class="form-group">
+    <label>折后价</label>
+    <input type="text" at-compare="price<=oPrice;price>lPrice" name="price" ng-model="item.price">
+  </div>
+</form>
+```
+
+### 以及其他对比...看用途
+
+## 如果条件不满足，则挂载在指令所在的表单按钮(通常是input)的``$error``上
+
+```html
+<form name="testForm">
+...
+<div class="form-group">
+    <label>原价</label>
+    <input type="text" at-compare="oPrice>lPrice" name="oPrice" ng-model="item.oProice">
+    <div ng-show="testForm.oPrice.$error['oPrice>lPrice']">
+        表单错误：原价必须要大于最低价
+    </div>
+</div>
+...
+</form>
+```
+
+# License
+项目遵循[MIT协议(The MIT License)](http://opensource.org/licenses/MIT)
--- at-compare.js
+++ at-compare.js
@@ -1,4 +1,4 @@
-(function (factory) {
+;(function (factory) {
   'use strict';
   var g = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : this;
   var module = g.module;
@@ -20,78 +20,111 @@
   var ｇ = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : this;
   var angular = ｇ.angular;
 
+  /**
+   * 匹配表达式的正则表达式
+   * @type {RegExp}
+   */
+  var EXPRESSION_REG = /^(\w+)\s*([\<\!\=\>]{1,3})\s*(\w+)$/i;
+
+  /**
+   * 不同语法执行的对比函数
+   */
+  var compare = {
+    '===': function (a, b) {
+      return a === b;
+    },
+    '==': function (a, b) {
+      return a == b;
+    },
+    '!==': function (a, b) {
+      return a !== b;
+    },
+    '!=': function (a, b) {
+      return a != b;
+    },
+    '>=': function (a, b) {
+      return parseFloat(a) >= parseFloat(b);
+    },
+    '>': function (a, b) {
+      return parseFloat(a) > parseFloat(b);
+    },
+    '<=': function (a, b) {
+      return parseFloat(a) <= parseFloat(b);
+    },
+    '<': function (a, b) {
+      return parseFloat(a) < parseFloat(b);
+    }
+  };
+
+  /**
+   * 支持的语法
+   * @type {string}
+   */
+  var supportSyntax = '["===","==","!==","!=",">=",">","<=","<"]';
+
   return angular.module('atCompare', [])
-    .directive('compare', function () {
-      var compare = {
-        '===': function (a, b) {
-          return a === b;
-        },
-        '==': function (a, b) {
-          return a == b;
-        },
-        '!==': function (a, b) {
-          return a !== b;
-        },
-        '!=': function (a, b) {
-          return a != b;
-        },
-        '>=': function (a, b) {
-          return parseFloat(a) >= parseFloat(b);
-        },
-        '>': function (a, b) {
-          return parseFloat(a) > parseFloat(b);
-        },
-        '<=': function (a, b) {
-          return parseFloat(a) <= parseFloat(b);
-        },
-        '<': function (a, b) {
-          return parseFloat(a) < parseFloat(b);
-        }
-      };
-
-      var supportSyntax = '["===","==","!==","!=",">=",">","<=","<"]';
-
+    .directive('atCompare', function () {
       return {
         restrict: 'A',
         require: 'ngModel',
-        scope: {},
         link: function postLink($scope, $element, $attrs, ctrl) {
 
-          var REG = /^(\w+)\s*([\<\!\=\>]{1,3})\s*(\w+)$/i;
-
+          /**
+           * 存放指令所含有的表达式
+           * @type {Array}
+           */
           var list = [];
 
+          /**
+           * 当前input所处在的父级表单
+           * @type {*|any}
+           */
+          var form = $element.inheritedData("$formController");
+
           $attrs.compare.split(';').forEach(function (v) {
+            var match,        // 匹配到的数组
+              obj = {};       // 临时哈希表
             if (!v) return;
+
             v = v.trim();
-            if (!REG.test(v)) {
+
+            if (!EXPRESSION_REG.test(v)) {
               console.error('Uncaught Syntax Error: 【%s】 is not the compare directive,please use %s', v, supportSyntax);
               return;
             }
-            var result = REG.exec(v);
-            var obj = {};
-            obj.source = result[1];
-            obj.mode = result[2];
-            obj.target = result[3];
+
+            match = EXPRESSION_REG.exec(v);
+
+            /**
+             * 表达式a>b
+             */
+            obj.source = match[1];    // a
+            obj.mode = match[2];      // >
+            obj.target = match[3];    // b
             list.push(obj);
           });
 
-          var form = $element.inheritedData("$formController");
-
           angular.forEach(list, function (v) {
 
-            var source = form[v.source];
-            var target = form[v.target];
+            /**
+             * 表达式a>b
+             */
+            var source = form[v.source];      // a
+            var target = form[v.target];      // b
 
-            var compareFn = compare[v.mode];
+            var compareFn = compare[v.mode];  // 对比函数
 
-            var syntax = v.source + v.mode + v.target;
+            var syntax = v.source + v.mode + v.target;    // 当前语法
 
             if (!compareFn) {
               console.error('Uncaught Syntax Error: 【%s】 is not the compare directive,please use %s', syntax, supportSyntax);
               return false;
             }
 
+            /**
+             * 给当前指令的input
+             * $setValidity
+             */
             ctrl.$parsers.push(function (viewValue) {
               ctrl.$setValidity(syntax, compareFn(source.$viewValue, target.$viewValue));
               return viewValue;
