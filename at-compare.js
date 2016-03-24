058f040b7fd5ca0c7bb94067e1881359ff7bd364
axetroy <troy450409405@gmail.com>
2016-03-24 21:35:20 +0800

    支持直接和数字/字符串/下的变量进行比较，修复捕获不到表单元素产生的bug，以及其他

--- at-compare.js
+++ at-compare.js
@@ -22,7 +22,7 @@
    * 匹配表达式的正则表达式
    * @type {RegExp}
    */
-  var EXPRESSION_REG = /^(\w+)\s*([\<\!\=\>]{1,3})\s*(\w+)$/i;
+  var EXPRESSION_REG = /^([\w\$\.]+)\s*(in \$?scope)?\s*([\<\!\=\>]{1,3})\s*(['|"]?[\w\$\.]+['|"]?)\s*(in \$?scope)?$/;
 
   /**
    * 不同语法执行的对比函数
@@ -40,6 +40,10 @@
     '!=': function (a, b) {
       return a != b;
     },
+    /**
+     *  所有如果a,b不是数字，或者parseFloat之后不是数字，结果会是NaN
+     *  任何东西和NaN比，都会是false
+     */
     '>=': function (a, b) {
       a = a || 0;
       b = b || 0;
@@ -69,26 +73,25 @@
   var supportSyntax = '["===","==","!==","!=",">=",">","<=","<"]';
 
   return angular.module('atCompare', [])
-    .directive('atCompare', ['$timeout', '$log', function ($timeout, $log) {
+    .directive('atCompare', ['$parse','$timeout','$log',function ($parse, $timeout, $log) {
       return {
         restrict: 'A',
         require: 'ngModel',
         link: function postLink($scope, $element, $attrs, ctrl) {
 
-          if (!ctrl) return;
-
-          /**
-           * 存放指令所含有的表达式
-           * @type {Array}
-           */
-          var list = [];
-
-          /**
-           * 当前input所处在的父级表单
-           */
-          var form = $element.inheritedData("$formController");
-
           $timeout(function () {
+            /**
+             * 存放指令所含有的表达式
+             * @type {Array}
+             */
+            var list = [];
+
+            /**
+             * 当前input所处在的父级表单
+             * @type {*|any}
+             */
+            var form = $element.inheritedData("$formController");
+
             $attrs.atCompare.split(';').forEach(function (v) {
               var match,        // 匹配到的数组
                 obj = {};       // 临时哈希表
@@ -107,8 +110,13 @@
                * 表达式a>b
                */
               obj.source = match[1];    // a
-              obj.mode = match[2];      // >
-              obj.target = match[3];    // b
+              obj.sourceInScope = !!match[2]; // 是否从scope拿变量
+
+              obj.mode = match[3];      // >
+
+              obj.target = match[4];    // b
+              obj.targetInScope = !!match[5]; // 是否从scope拿变量
+
               list.push(obj);
             });
 
@@ -117,34 +125,68 @@
               /**
                * 表达式a>b
                */
-              var source = form[v.source];      // a
-              var target = form[v.target];      // b
-
-              var compareFn = compare[v.mode];  // 对比函数
+              var source = v.source;
+              var target = v.target;
 
               var syntax = v.source + v.mode + v.target;    // 当前语法
+
+              // 如果是$scope下的变量
+              if (!!v.sourceInScope) {
+                source = v.source = $parse(v.source)($scope);
+              } else {
+                source = v.source;
+                // 如果是数字
+                if (/^\s*[\d\.?\d)]+\s*$/.test(v.source)) {
+                  source = parseFloat(v.source);
+                }
+                // 如果是字符串
+                else if (/^['|"][\w\$\.]+['|"]$/.test(v.source)) {
+                  source = source.replace(/^['|"](.*)['|"]$/, '$1');
+                }
+                // 默认是name值
+                else {
+                  source = form[v.source];
+                }
+              }
+
+
+              if (!!v.targetInScope) {
+                target = v.target = $parse(v.target)($scope);
+              } else {
+                target = v.target;
+                if (/^\s*[\d\.?\d)]+\s*$/.test(v.target)) {
+                  target = parseFloat(v.target);
+                }
+                else if (/^['|"][\w\$\.]+['|"]$/.test(v.target)) {
+                  target = target.replace(/^['|"](.*)['|"]$/, '$1');
+                }
+                else {
+                  target = form[v.target];
+                }
+              }
+
+              var compareFn = compare[v.mode];  // 对比函数
 
               if (!compareFn) {
                 $log.error('Uncaught Syntax Error: [%s] is not the compare directive,please use %s || %s', syntax, supportSyntax, $element[0].outerHTML);
                 return false;
               }
 
-              /**
-               * 给当前指令的input
-               * $setValidity
-               */
               ctrl.$parsers.push(function (viewValue) {
-                ctrl.$setValidity(syntax, compareFn(source.$viewValue, target.$viewValue));
+                ctrl.$setValidity(syntax, compareFn(source.$parsers ? source.$viewValue : source, target.$parsers ? target.$viewValue : target));
                 return viewValue;
               });
 
-              target.$parsers.push(function (viewValue) {
-                ctrl.$setValidity(syntax, compareFn(source.$viewValue, target.$viewValue));
-                return viewValue;
-              });
+              if (angular.isObject(target) && angular.isArray(target.$parsers)) {
+                target.$parsers.push(function (viewValue) {
+                  ctrl.$setValidity(syntax, compareFn(source.$parsers ? source.$viewValue : source, target.$parsers ? target.$viewValue : target));
+                  return viewValue;
+                });
+              }
 
             });
-          }, 0);
+
+          });
 
         }
       };
